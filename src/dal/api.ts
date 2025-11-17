a9bdceafe8a335c931d6e6d6dc74a5f56b858159
Naberezhniy Vasiliy <89585704814v@gmail.com>
2025-11-17 11:44:51 +0300

    add env for api-key

--- src/dal/api.ts
+++ src/dal/api.ts
@@ -6,13 +6,19 @@ export type GetTrackDetailsOutputData = {
     lyrics: string | null
   }
 }
+const prepareHeaders = () => {
+  const apiKey = import.meta.env.VITE_API_KEY
+  if(!apiKey) return  undefined
+
+  return {
+    'api-key': apiKey
+  }
+}
 export const getTrack = (trackId: string) => {
   const promise: Promise<{
     data: GetTrackDetailsOutputData
   }> = fetch("https://musicfun.it-incubator.app/api/1.0/playlists/tracks/" + trackId, {
-    headers: {
-      // 'api-key': '9fcf0d75-36d0-4680-b175-91549b20920a'
-    }
+    headers: prepareHeaders()
   })
     .then(res => res.json())
   return promise
@@ -33,11 +39,10 @@ export type TrackListItemOutput = {
 type GetTrackListOutput = {
   data: Array<TrackListItemOutput>
 }
+
 export const getTracks = () => {
   const promise: Promise<GetTrackListOutput> = fetch("https://musicfun.it-incubator.app/api/1.0/playlists/tracks", {
-    headers: {
-      // 'api-key': '9fcf0d75-36d0-4680-b175-91549b20920a'
-    },
+    headers: prepareHeaders()
   })
     .then(res => res.json())
   return promise
